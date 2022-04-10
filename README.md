54ec6a37e6d5ffe16a1dec6aad72cab72bfdf892
guilherssousa <guilherssousa@gmail.com>
2022-04-10 00:10:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Water
+
+Water é uma ferramenta para observar alterações em arquivos para otimizar sua experiência de desenvolvimento.
+
+### Sobre
+
+Water é um projeto criado por um programador Javascript, que casualmente utiliza a linguagem Go para construir pequenos projetos de automação e afins. Portanto, não deve ser usado de forma alguma em projetos de produção ou de qualquer outra que necessite de contribuição e manutenção de código de seus desenvovedores.
+
+### Como usar
+
+Primeiro, compile o código usando o comando `go build main.go`.
+
+Depois, execute rodando o programa com o comando `./main`e passando os argumentos da seguinte forma:
+
+```bash
+    ./main <diretorio-a-assistir>
+```
---
+++ main.go
@@ -0,0 +1,57 @@
+// This file is a tooling built to help on the overlay design process
+
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	watcher, err := fsnotify.NewWatcher();
+
+	if err != nil {
+		log.Fatal(err);
+	}
+
+	done := make(chan bool);
+
+	go func() {
+		defer close(done)
+
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Printf("%s %s\n", event.Name, event.Op)
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	dir, err := os.Getwd();
+	inputPath := os.Args[1];
+
+	if err != nil {
+		log.Fatal(err);
+	}
+
+	finalPath := filepath.Join(dir, inputPath)
+
+	log.Println("Watching: ", finalPath);
+
+	err = watcher.Add(finalPath)
+	if err != nil {
+		log.Fatal("Add failed: ", err)
+	}
+	<-done
+}
